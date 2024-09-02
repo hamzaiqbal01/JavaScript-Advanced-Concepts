6c19d3929a09fd88b306a1db16ba4b0a20964a13
Hamza Iqbal <97678599+hamzaiqbal01@users.noreply.github.com>
2024-09-01 23:53:33 -0700

    define execution context

--- How JavaScript Work behind the Scene/README.md
+++ How JavaScript Work behind the Scene/README.md
@@ -1,6 +1,7 @@
 
 
-##  🚀 How JavaScript works behind the scene🧑🏾‍💻
+## How JavaScript works behind the scene🧑🏾‍💻
+
 
 Every thing in js happends inside an execution context
 
@@ -8,3 +9,15 @@ Every thing in js happends inside an execution context
 
 ## Execution context: 
 JavaScript Execution Context is the environment in which JavaScript code is executed. It contains information about the variables, functions, and objects that are available to the code being executed, as well as the scope chain and the value of the 'this' keyword.
+## Phases 🪜
+
+This project is used by the following companies:
+
+- Memory Creation and Memory allocation phase phase 📝
+- Code Execution phase 
+
+
+## Screenshots
+
+![App Screenshot](https://snipboard.io/IzB4AO.jpg)
+
--- How JavaScript Work behind the Scene/script.js
+++ How JavaScript Work behind the Scene/script.js
@@ -11,17 +11,68 @@
 
 
 // How functions work in js
-var name = "hamza";
-student1();
-student2();
-console.log(name)
+// var name = "hamza";
+// student1();
+// student2();
+// console.log(name)
 
-function student1() {
-    let name = "taimoor";
-    console.log(name)
-}
+// function student1() {
+//     let name = "taimoor";
+//     console.log(name)
+// }
 
-function student2() {
-    let name = "zahid";
-    console.log(name)
+// function student2() {
+//     let name = "zahid";
+//     console.log(name)
+// }
+
+
+// var a = 1;
+
+
+
+// // lexical scope:
+
+
+// function calculator() {
+//     var userInput = 10;
+//     function sum() {
+//         console.log(`user input is = ${userInput}`);
+//     }
+//     sum();
+// }
+
+// calculator();
+
+
+// let a;
+//  console.log(a);
+//  var b;
+
+
+// var x = 10;
+
+// {
+//     var x = 20;
+//     let b = 30;
+// }
+// console.log(x)
+
+
+// function x() {
+//     var i = 10;
+//     setTimeout(() => {
+//         console.log(i)
+//     }, 2000);
+// }
+// x();
+
+
+function print() {
+    for (let i = 0; i <= 5; i++) {
+        setTimeout(() => {
+            console.log(i);
+        }, i * 1000)
+    }
 }
+print();
